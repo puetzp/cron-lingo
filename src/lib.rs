a4c9bb1baf2d7035527e99204535880e6deeae7d
puetzp <peter.puetz@itenos.de>
2021-01-28 14:57:35 +0100

    Add module-level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! Small utility to parse a cron-like expression in human-readable format and
+//! iterate over upcoming dates according to this schedule.
+//! # Example
+//! ```rust
+//! use cron_lingo::Timetable;
+//!
+//! fn main() {
+//!     let expr = "at 9 o'clock on Monday and Friday";
+//!     let timetable = Timetable::new(expr).unwrap();
+//!     assert!(timetable.into_iter().next().is_some());
+//! }
+//! ```
 pub mod error;
 pub mod timetable;
 
